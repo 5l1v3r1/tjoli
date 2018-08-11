9ead16f21614ab3f41a86f47b16721ec633eb72d
siputra12 <siputra0770@gmail.com>
2018-08-11 17:42:48 +0700

    Update pakan.php

--- pakan.php
+++ pakan.php
@@ -35,8 +35,8 @@ $jumlah = trim(fgets(STDIN));
 echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
 $jeda = trim(fgets(STDIN));
 for($a=0;$a<$jumlah;$a++){
-	$jum = rand(123,987);
-	$jum1 = rand(123,987);
+	$jum = rand(123,$jumlah);
+	$jum1 = rand($jum,$jumlah);
 	$chance = jolly($uid, $usertoken, $jum);
 	$chance1 = jolly($uid, $usertoken, $jum1);
 	$pakan = pakan($uid, $usertoken);
